10036120c452874b11826ce6e8f4e59c8cffae3e
PuzzleZa <118202603+PuzzleZa@users.noreply.github.com>
2022-11-15 16:14:28 +0800

    Update create_database.sql

--- dump/create_database.sql
+++ dump/create_database.sql
@@ -1,6 +1,30 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 --SET time_zone = "+08:00";
 
+CREATE TABLE 'users' (
+  'id' int(11) NOT NULL,
+  'username' varchar(255) NOT NULL,
+  'email' varchar(255) NOT NULL,
+  'password' varchar(155) NOT NULL,
+  PRIMARY KEY ('id')
+) ENGINE=InnoDB;
+
+--
+-- Contenu de la table 'users'
+--
+
+INSERT INTO 'users' ('id', 'username', 'email', 'password') VALUES
+(1, 'bob', 'bob@example.fr', '26588e932c7ccfa1df309280702fe1b5'),
+(4, 'vsk', 'vsk@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055'),
+(6, 'database', 'database@free.fr', '11e0eed8d3696c0a632f822df385ab3c'),
+(7, 'db', 'db@free.fr', 'd77d5e503ad1439f585ac494268b351b'),
+(9, 'admin', 'admin@free.fr', '21232f297a57a5a743894a0e4a801fc3'),
+(13, 'esigelec', 'esigelec@free.fr', '81dc9bdb52d04dc20036dbd8313ed055'),
+(14, 'kumar', 'kumar@gmail.com', '79cfac6387e0d582f83a29a04d0bcdc4'),
+(15, 'admins', 'admin@esigelec.fr', '2aefc34200a294a3cc7db81b43a81873'),
+(16, 'today', 'today@esigelec.fr', 'c5e7dfaf771d423ecf59b008369021e8'),
+(17, 'peter', 'peter@esigelec.fr', '51dc30ddc473d43a6011e9ebba6ca770');
+
 --
 -- create table 'orders'
 --
@@ -62,4 +86,9 @@ ALTER TABLE 'products'
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
 
--- 
+-- create role
+CREATE ROLE 'admin'@'localhost';
+GRANT DELETE, SELECT ON *.* TO 'admin'@'localhost';
+
+create role 'user'@'localhost';
+GRANT INSERT ON *.* TO 'admin'@'localhost';
