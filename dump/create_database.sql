73e40b486d365274a2f813436622b596c8dfa114
lossfuture <82207903+lossfuture@users.noreply.github.com>
2022-11-19 10:06:57 +0800

    Update create_database.sql

--- dump/create_database.sql
+++ dump/create_database.sql
@@ -107,6 +107,8 @@ INSERT INTO `products` (`id`, `image`, `category`, `product`, `price`) VALUES
 ALTER TABLE `products`
   MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
+CREATE TABLE `verification_code` (`id` INT NOT NULL AUTO_INCREMENT , `user_id` INT NOT NULL , `user_type` TEXT NOT NULL , `verification_code` INT NOT NULL , `expried_date` DATETIME NOT NULL ) 
+ENGINE = InnoDB;
 
 
 commit;
