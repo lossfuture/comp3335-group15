43912fa27ea66dfee9d18428937a6b1b7e88338c
PuzzleZa <118202603+PuzzleZa@users.noreply.github.com>
2022-11-19 21:57:22 +0800

    Update server_admin.php

--- www/admin/server_admin.php
+++ www/admin/server_admin.php
@@ -1,30 +1,7 @@
 <?php 
-
-function pdoConnection() {
-
-    try {
-
-        $db_name     = 'comp3335';
-        $db_user     = 'root';
-        $db_password = 'Ip38w#*5tA%hwJLy';
-        $db_host     = 'db:3306';
-
-        $pdo = new PDO('mysql:host=' . $db_host . '; dbname=' . $db_name, $db_user, $db_password);
-
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        return $pdo;
-
-
-
-       } catch (PDOException $e) {
-
-           echo $e->getMessage();
-
-       }
-
-}
-$pdo = pdoConnection();
+require("../pdoAdmin.php");
+$pdoAdmin = new pdoadmin();
+$pdo = $pdoAdmin-> pdoConnection();
 	
 
 	// variable declaration
