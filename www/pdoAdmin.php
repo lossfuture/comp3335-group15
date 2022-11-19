d69a33d89045d4793dc71f85e6f477a3ff4dc5d1
PuzzleZa <118202603+PuzzleZa@users.noreply.github.com>
2022-11-19 23:38:28 +0800

    Update pdoAdmin.php

--- www/pdoAdmin.php
+++ www/pdoAdmin.php
@@ -4,8 +4,8 @@ class pdoadmin{
     try {
 
         $db_name     = 'comp3335';
-        $db_user     = 'validator1';
-        $db_password = '87fd89asf02';
+        $db_user     = 'admin1';
+        $db_password = '1115597898AAAb';
         $db_host     = 'db:3306';
 
         $pdo = new PDO('mysql:host=' . $db_host . '; dbname=' . $db_name, $db_user, $db_password);
