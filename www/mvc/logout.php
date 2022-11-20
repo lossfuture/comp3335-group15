e24496892cd96de0f10d1736e0dc38fa87e42954
lossfuture <82207903+lossfuture@users.noreply.github.com>
2022-11-20 20:30:22 +0800

    Update logout.php

--- www/mvc/logout.php
+++ www/mvc/logout.php
@@ -14,10 +14,15 @@
 	// connect to database
 //$connect = mysqli_connect('127.0.0.1', 'root', '12581258', 'user-registration');
 //include_once("logout.php");
+unset($_SESSION["2fa"]);
+unset($_SESSION['username']);
+
 if (isset($_COOKIE["user"])) {
 	unset($_COOKIE["user"]);
+
 	setcookie("user","",time()-1800);
-	header("Location: home.php");
+	
 }
+header("Location: home.php");
 //echo <script> location.href="userLogin.php"</script>;
 ?>
