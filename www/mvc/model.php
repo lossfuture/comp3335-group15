455fa8a1386921cffef5490f39bd830ddcb4ce5b
lossfuture <82207903+lossfuture@users.noreply.github.com>
2022-11-20 20:32:30 +0800

    Update model.php

--- www/mvc/model.php
+++ www/mvc/model.php
@@ -1,4 +1,5 @@
 <?php session_start();
+require_once("send-email.php");
 
 class model
 {
@@ -16,6 +17,92 @@ class model
         $this->ds = new datasource();
     }
 
+       public function generate_code(){
+        return substr(number_format(time() * rand(), 0, '', ''), 0, 6);
+    }
+
+    public function verify_token($token){
+        
+        $query = 'SELECT user_id FROM email_verfiication where token = ? LIMIT 1';
+        $paramType = 's';
+        $paramValue = array(
+            $token
+        );
+        $insertRecord = $this->ds->select($query, $paramType, $paramValue);
+        //var_dump($insertRecord);
+        $count = 0;
+        if (is_array($insertRecord)) {
+            $count = count($insertRecord);
+        }else{
+            echo "Error: token not exist";
+            return;
+        }
+
+        $query = 'UPDATE users SET email_verified = 1 where id = ?';
+        $paramType = 'i';
+        $paramValue = array(
+            $insertRecord[0]["user_id"]
+        );
+        //var_dump($paramValue);
+        $insertRecord = $this->ds->execute($query, $paramType, $paramValue);
+        //var_dump($insertRecord);
+
+            echo "<a href=\"login-form.php\"><p>You can login now.</p></a>";
+
+
+        
+        
+    }
+
+   
+
+        
+
+    
+    public function send_2fa_code($id_value,$email){
+        
+        $today_time=new DateTime();
+        $timezone = new DateTimeZone('Asia/Hong_Kong');
+        $today_time->setTimezone($timezone);
+
+        $today_time->add(new DateInterval('PT10M'));
+        $today_time=$today_time->format("Y-m-d H:i:s");
+
+        $token=$this->generate_code();
+        $query = 'INSERT INTO verification_code (user_id, user_type ,verification_code, expried_date) VALUES (?, "user",?, ?)';
+        $paramType = 'iis';
+        $paramValue = array(
+            $id_value,
+            $token,
+     //       $email,
+            $today_time, 
+        );
+
+        //var_dump( $paramValue);
+
+        $memberId = $this->ds->insert($query, $paramType, $paramValue);
+        send_email($email,$token);
+
+    }
+
+    public function verify_2fa_code($token){
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+        $query = 'SELECT user_id FROM verification_code where verification_code = ? LIMIT 1';
+        $paramType = 'i';
+        $paramValue = array(
+            $token
+        );
+        $insertRecord = $this->ds->select($query, $paramType, $paramValue);
+
+        $count = 0;
+        if (is_array($insertRecord)) {
+            $_SESSION["2fa"] = TRUE;
+            return true;
+        }else{
+            return false;
+        }
+
+        
     public function isMemberExists($email)
     {
         $query = 'SELECT * FROM users where email = ?';
@@ -67,8 +154,30 @@ class model
             );
 
             $memberId = $this->ds->insert($query, $paramType, $paramValue);
+            $token=hash('sha256', $this->generate_code());
+
+            $today_time=new DateTime();
+            $timezone = new DateTimeZone('Asia/Hong_Kong');
+            $today_time->setTimezone($timezone);
+
+            $today_time->add(new DateInterval('PT1H'));
+            $today_time=$today_time->format("Y-m-d H:i:s");
+            
+
+            $query2 = 'INSERT INTO email_verfiication (id,token, expried_date, user_id ) VALUES (? ,? , ?, ?)';
+            $paramType2 = 'issi';
+            $paramValue2 = array(
+                NULL,
+                $token,
+                $today_time,
+                $memberId
+            );
+
+            $result2 = $this->ds->insert($query2, $paramType2, $paramValue2);
+
+            send_email($_POST["email"],$token);
             if($memberId) {
-                $response = array("status" => "success", "message" => "You have registered successfully.");
+                $response = array("status" => "success", "message" => "You have registered successfully.<br>An verification email has been sent, plesase verify <a href=\"email-verify.php\" > here</a>");
             }
             else{
                 $response = array("status" => "error", "message" => "cant be empty in * column");
@@ -122,7 +231,13 @@ class model
         }
         if ($loginPassword == 1 && $_SESSION['attempt'] < 3) {
             $_SESSION["username"] = $loginUserResult[0]["username"];
+            $_SESSION["2fa"] = FALSE;
+            $_SESSION["user_id"] =  $loginUserResult[0]["id"];
             setcookie("user","user",time()+1800);
+            
+            // 2fa
+            $this->send_2fa_code( $loginUserResult[0]["id"], $loginUserResult[0]["email"]);
+            
             $loginurl = 'in.php';
             //require_once $loginurl;
             header("Location: $loginurl");
