55588d783ba9de868c2e1f7efa08e963319aa4af
lossfuture <82207903+lossfuture@users.noreply.github.com>
2022-11-20 20:29:23 +0800

    Update email-verify.php

--- www/mvc/email-verify.php
+++ www/mvc/email-verify.php
@@ -4,12 +4,20 @@ require __DIR__.'/../mvc/model.php';
 
     //Use get instad of post for verify
 
+        if (!isset($_GET["token"])){
+            echo'
+            <form method="GET">
+    <input type="text" name="token" placeholder="Enter verification code" required />
+    <input type="submit" name="verify_email" value="Verify Email">
+</form>';
+            exit();
+        }
         $token = $_GET["token"]; //md5 hash
-        $verification_code = $_GET["verification_code"]; //number
+
  
         // connect with database
         $member = new model();
-        $loginResult = $member-> verifytoken();
+        $loginResult = $member-> verify_token($token);
  
         // mark email as verified
         $current_time = date("Y-m-d H:i:s");
@@ -19,14 +27,14 @@ require __DIR__.'/../mvc/model.php';
         //verify email if not yet verify yet
 
         //$sql = "UPDATE users SET email_verified_at = NOW() WHERE email = '" . $email . "' AND verification_code = '" . $verification_code . "'";
-        $result  = mysqli_query($conn, $sql);
+  //      $result  = mysqli_query($conn, $sql);
  
-        if (mysqli_affected_rows($conn) == 0)
-        {
-            die("Verification code failed.");
-        }
+   //     if (mysqli_affected_rows($conn) == 0)
+  //      {
+   //         die("Verification code failed.");
+   //     }
  
-        echo "<p>You can login now.</p>";
+  //      echo "<p>You can login now.</p>";
         exit();
     
  
